38af2939a095550d9b17cf8f30def7dd29eb2589
bernardkaminskibk <bernardkaminski.bk@gmail.com>
2023-06-01 18:00:03 +0200

    Added new functionality

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,8 @@ public class Main {
 
     public static void main(String[] args) {
         System.out.println("Priklad na pracu s vzdialenymi repozitarmi");
+
+        System.out.println("nova funkcionalita"); // toto akoze nieco robi
     }
 
 }
